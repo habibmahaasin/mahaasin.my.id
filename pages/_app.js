56dd312cc35cb789bc2a8a3659f9553614be701d
Habibmahaasin <habibirf23@gmail.com>
2023-01-21 02:42:34 +0700

    update head section

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,16 @@ import '../styles/globals.css'
 import Navigation from 'components/layouts/navigation'
 import Header from 'components/layouts/header'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Head from 'next/head'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
   return(
     <> 
+      <Head>
+        <title>Sin - Portofolio</title>
+        <link rel="shortcut icon" href="/static/favicon.ico" />
+      </Head>
       <Header/>
       <div className='background-content'>
         <div className='container-web-app container-fluid px-4'>
