abba07d73769609a0b06631058c9b00f1038d3f6
Habibmahaasin <habibirf23@gmail.com>
2023-01-21 15:43:30 +0700

    fixing header

--- components/layouts/header.js
+++ components/layouts/header.js
@@ -2,9 +2,9 @@ import Image from "next/image"
 
 export default function Header(){
     return(
-      <nav className="navbar fixed-top bg-header">
+      <nav className="navbar fixed-top bg-header p-0">
         <div className="header-web-app container-fluid d-flex justify-content-between">
-          <Image src={`/logo.svg`} className='image' height='42' width='42' alt="him logo" />
+          <Image src={`/logo.svg`} className='image' height='34' width='34' alt="him logo" />
           <p className="copyright-head mt-3">
               <span>&#169; mahaasin.my.id 2023</span>
           </p>
